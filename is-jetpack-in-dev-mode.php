38bac3e4fc5e98d4ec414bc1ce5b173f1f156fed
Brandon Kraft <public@brandonkraft.com>
2014-02-25 08:32:04 -0600

    * Move notice to jetpack_notices * Change name to reflect GitHub slug

--- is-jetpack-in-dev-mode.php
+++ is-jetpack-in-dev-mode.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://brandonkraft.com
  * Description: Is Jetpack in dev mode? Why? Who goes there?
  * Author: Brandon Kraft
- * Version: 1.0-beta1
+ * Version: 1.0-beta2
  * Author URI: http://brandonkraft.com
  * License: GPL2+
  *
@@ -12,12 +12,12 @@
 
 function bk_jp_is_development_mode() {
 	$development_mode = false;
-	$development_mode_status = 'JETPACK: Not in Development Mode';
+	$development_mode_status = 'Not in Development Mode';
 	$notice_status = "updated";
 
 	if ( defined( 'JETPACK_DEV_DEBUG' ) ) {
 		if ( JETPACK_DEV_DEBUG == true) {
-			$development_mode_status = "JETPACK: In Dev Mode via wp-config.php or constant being defined elsewhere.";
+			$development_mode_status = "In Dev Mode via wp-config.php or constant being defined elsewhere.";
 			$notice_status = "error";
 		}
 	}
@@ -28,7 +28,7 @@ function bk_jp_is_development_mode() {
 	}
 
 	if ( apply_filters( 'jetpack_development_mode', $development_mode ) == true ) {
-		$development_mode_status = "JETPACK: In Dev Mode via an add_filter call in a plugin.";
+		$development_mode_status = "In Dev Mode via an add_filter call in a plugin.";
 		$notice_status = "error";
 	}
 
@@ -39,4 +39,4 @@ function bk_jp_is_development_mode() {
 	<?php
 }
 
-add_action( 'admin_notices', 'bk_jp_is_development_mode' );
+add_action( 'jetpack_notices', 'bk_jp_is_development_mode' );
