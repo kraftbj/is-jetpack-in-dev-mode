2a55304ee394efed7ee65a7d1e464d65bae97708
Gary Jones <GaryJones@users.noreply.github.com>
2014-02-26 14:42:07 +0000

    Documentation, coide standards, refactoring
    1. Documentation added.
    2. Code standards (using single quotes where possible, three-digit version number)
    3. Split function into two - one that gets the data, and one that does something with it.
    4. Bumped version to 1.0.0-beta3.

--- is-jetpack-in-dev-mode.php
+++ is-jetpack-in-dev-mode.php
@@ -1,42 +1,70 @@
 <?php
-/*
- * Plugin Name: Is Jetpack in Dev Mode?
- * Plugin URI: http://brandonkraft.com
- * Description: Is Jetpack in dev mode? Why? Who goes there?
- * Author: Brandon Kraft
- * Version: 1.0-beta2
- * Author URI: http://brandonkraft.com
- * License: GPL2+
+/**
+ * Is Jetpack in Dev Mode
  *
+ * @package     Is_Jetpack_In_Dev_Mode
+ * @author      Brandon Kraft
+ * @author      Gary Jones
+ * @link        https://github.com/kraftbj/is-jetpack-in-dev-mode
+ * @copyright   2014 Brandon Kraft
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: Is Jetpack in Dev Mode?
+ * Plugin URI:  https://github.com/kraftbj/is-jetpack-in-dev-mode
+ * Description: Is Jetpack in dev mode? Why? Who goes there?
+ * Version:     1.0.0-beta3
+ * Author:      Brandon Kraft
+ * Author URI:  http://brandonkraft.com
+ * Text Domain: is-jetpack-in-dev-mode
+ * Domain Path: /languages
+ * License:     GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-function bk_jp_is_development_mode() {
-	$development_mode = false;
-	$development_mode_status = 'Not in Development Mode';
-	$notice_status = "updated";
+/**
+ * Get Jetpack development mode notice text and notice class.
+ *
+ * Mirrors the checks made in Jetpack::is_development_mode
+ *
+ * @since 1.0.0
+ *
+ * @return array Notice text and class.
+ */
+function bk_jp_get_development_mode_notice() {
+	$notice = __( 'Not in Development Mode', 'is-jetpack-in-dev-mode' );
+	$class  = 'updated';
 
-	if ( defined( 'JETPACK_DEV_DEBUG' ) ) {
-		if ( JETPACK_DEV_DEBUG == true) {
-			$development_mode_status = "In Dev Mode via wp-config.php or constant being defined elsewhere.";
-			$notice_status = "error";
-		}
+	if ( defined( 'JETPACK_DEV_DEBUG' ) && JETPACK_DEV_DEBUG ) {
+		$notice = __( 'In Dev Mode, via wp-config.php or constant being defined elsewhere.', 'is-jetpack-in-dev-mode' );
+		$class  = 'error';
+	} elseif ( site_url() && false === strpos( site_url(), '.' ) ) {
+		$notice = __( 'In Dev Mode, via site URL lacking a dot', 'is-jetpack-in-dev-mode' );
+		$class  = 'error';
 	}
 
-	elseif ( site_url() && false === strpos( site_url(), '.' ) ) {
-		$development_mode_status = "In Dev Mode via site URL lacking a dot";
-		$notice_status = "error";
+	/** This filter should be documented in jetpack/class.jetpack.php */
+	if ( apply_filters( 'jetpack_development_mode', false ) ) {
+		$notice = __( 'In Dev Mode, via an add_filter call in a plugin.', 'is-jetpack-in-dev-mode' );
+		$class  = 'error';
 	}
 
-	if ( apply_filters( 'jetpack_development_mode', $development_mode ) == true ) {
-		$development_mode_status = "In Dev Mode via an add_filter call in a plugin.";
-		$notice_status = "error";
-	}
-
-	?>
-	<div class="<?php echo $notice_status;?>">
-		<p><?php echo $development_mode_status;?></p>
-	</div>
-	<?php
+	return compact( $notice, $class );
 }
 
 add_action( 'jetpack_notices', 'bk_jp_is_development_mode' );
+/**
+ * Show Jetpack development mode notice.
+ *
+ * @since 1.0.0
+ * 
+ * @uses bk_jp_get_development_mode_notice() Get Jetpack development mode notice text and notice class.
+ */
+function bk_jp_show_development_mode_notice() {
+	list( $notice, $class ) = bk_jp_get_development_mode_notice();
+	?>
+	<div class="<?php echo $class;?>">
+		<p><?php echo $notice;?></p>
+	</div>
+	<?php
+}
